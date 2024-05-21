af63df6b6ca9665573fb5505ec65ab80da5b8cbc
elmarkazumov <elmarkazumov20@gmail.com>
2024-05-21 19:47:51 +0500

    sync move elements, generate square

--- frontend/resources/app/game.js
+++ frontend/resources/app/game.js
@@ -1,42 +1,81 @@
 const canvas = document.getElementById('app__render-zone');
 const context = canvas.getContext('2d');
 
-const squareParam = {
-    x: 10,
-    y: 10,
-    w: 50,
-    h: 50,
-    moveTo (axisAdd){
-        context.clearRect(this.x, this.y, this.w, this.h);
-    
-        if(axisAdd === 'x+') this.x++;
-    
-        if(axisAdd === 'x-') this.x--;
-    
-        if(axisAdd === 'y+') this.y++;
-    
-        if(axisAdd === 'y-') this.y--;
-    
-        context.fillRect(this.x, this.y, this.w, this.h);
+canvas.height = 500;
+
+const randomSquare = {
+    createButton: document.getElementById('app__random-create'),
+    generateSquare(){
+        this.square = new Element(Math.random() * 200, Math.random() * 400, 50, 50);
+        context.fillRect(this.square.x, this.square.y, this.square.w, this.square.h);
     }
 }
 
-context.fillRect(squareParam.x, squareParam.y, squareParam.w, squareParam.h);
+function Element(x, y, w, h){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+}
+
+function syncMoveSquares(n){
+    const elements = Array();
+    let distance = 5;
+    for (let i = 0; i < n; i++) {
+        elements.push(new Element(distance, 10, 50, 50));
+        distance += 55;
+    }
+    
+    setInterval(() => {    
+        for(const el of elements){
+            context.clearRect(el.x, el.y, el.w, el.h);
+
+            if(el.y < canvas.height){
+                el.y++;
+            } else {
+                el.y = 0;
+            }
+
+            context.fillRect(el.x, el.y, el.w, el.h);
+        }
+    }, 20);
+}
+
+syncMoveSquares(2);
+
+randomSquare.createButton.addEventListener('click', () => {
+    randomSquare.generateSquare();
+})
+
+
+function moveTo(element, axisAdd){
+    context.clearRect(element.x, element.y, element.w, element.h);
+
+    if(axisAdd === 'x+') element.x++;
+
+    if(axisAdd === 'x-') element.x--;
+
+    if(axisAdd === 'y+') element.y++;
+
+    if(axisAdd === 'y-') element.y--;
+
+    context.fillRect(element.x, element.y, element.w, element.h);
+}
 
 document.addEventListener("keydown", (event) => {
     if(event.key === 'ArrowLeft'){
-        squareParam.moveTo('x-');
+        moveTo(randomSquare.square, 'x-');
     }
 
     if(event.key === 'ArrowRight'){
-        squareParam.moveTo('x+');
+        moveTo(randomSquare.square, 'x+');
     }
 
     if(event.key === 'ArrowUp'){
-        squareParam.moveTo('y-');
+        moveTo(randomSquare.square, 'y-');
     }
 
     if(event.key === 'ArrowDown'){
-        squareParam.moveTo('y+');
+        moveTo(randomSquare.square, 'y+');
     }
 });
