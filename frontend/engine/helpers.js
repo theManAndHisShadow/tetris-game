e755a15e912eafa73ebbf8d10ae2259fc2e2d315
plvs vltra <justemailpostbox@gmail.com>
2024-05-30 00:23:06 +0500

    [Helpers]: Added 'rotate()' function

--- frontend/engine/helpers.js
+++ frontend/engine/helpers.js
@@ -4,4 +4,13 @@ function getRandomNumber(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
 
     return Math.floor(rand);
-  }
+}
+
+function rotate(cx, cy, x, y, angle) {
+  var radians = (Math.PI / 180) * angle,
+      cos = Math.cos(radians),
+      sin = Math.sin(radians),
+      nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
+      ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
+  return {x: nx, y: ny};
+}
