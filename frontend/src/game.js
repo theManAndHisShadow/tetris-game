18551f2bb52052029d0a6899ced1dc3fe428c445
theManAndHisShadow <justemailpostbox@gmail.com>
2024-06-22 17:28:48 +0500

    [Style]: '.opacity-30' replaced with '.opacity-60'

--- frontend/src/game.js
+++ frontend/src/game.js
@@ -487,13 +487,13 @@ const Game = function({screenElement, fieldSize, gridCellSize, settings, devSett
             pause: function(){
                 this.states.isGamePaused = true;
                 console.log(this.screen.html);
-                this.screen.html.classList.add('opacity-30');
+                this.screen.html.classList.add('opacity-60');
                 console.log("[Game]: Current instance of game is paused");
             },
 
             resume: function(){
                 this.states.isGamePaused = false;
-                this.screen.html.classList.remove('opacity-30');
+                this.screen.html.classList.remove('opacity-60');
                 console.log("[Game]: Current instance of game is resumed");
             },
 
