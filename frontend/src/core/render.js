37b4ab51205017d80a1ab8f06a8bf7cc0908a78c
theManAndHisShadow <justemailpostbox@gmail.com>
2024-06-23 21:07:12 +0500

    [Renderer]: added '.drawGradientLine()' method

--- frontend/src/core/render.js
+++ frontend/src/core/render.js
@@ -22,7 +22,7 @@ const Renderer = function({context}) {
 
 
             /**
-             * Draws a lint form point A (x1, y1) to point B (x2, y2)
+             * Draws a line from point A (x1, y1) to point B (x2, y2)
              * @param {number} x1 position at x axis of start point (A)
              * @param {number} y1 position at y axis of start point (A)
              * @param {number} x2 position at x axis of start point (B)
@@ -44,6 +44,47 @@ const Renderer = function({context}) {
                 this.context.fill();
             },
 
+            /**
+             * Draws a gradient line from point A (x1, y1) to point B (x2, y2) using gradient color stops
+             * @param {number} x1 position at x axis of start point (A)
+             * @param {number} y1 position at y axis of start point (A)
+             * @param {number} x2 position at x axis of start point (B)
+             * @param {number} y2 position at y axis of start point (B)
+             * @param {number} w thickoness of line
+             * @param {object} gradientCoords coordinate of gradient starting, endong points
+             * @param {string} colorStop1 first color stop for gradient
+             * @param {string} colorStop2 second color stop for gradient
+             */
+            drawGradientLine: function({x1, y1, x2, y2, w, gradientCoords, colorStop1, colorStop2} = {}){
+                gradientCoords = gradientCoords || {x1: 0, y1: 0, x2: 0, y2: 100}
+                colorStop1 = colorStop1 || 'red';
+                colorStop2 = colorStop2 || 'blue';
+
+                const gradient = this.context.createLinearGradient(
+                    gradientCoords.x1, 
+                    gradientCoords.y1, 
+                    gradientCoords.x2, 
+                    gradientCoords.y2,
+                );
+
+                gradient.addColorStop(0, colorStop1);
+                gradient.addColorStop(1, colorStop2);
+
+                this.context.strokeStyle = gradient;
+                this.context.fillStyle = gradient;
+                this.context.lineWidth = w;
+
+                this.context.beginPath();
+                this.context.moveTo(x1, y1);
+                this.context.lineTo(x2, y2);
+                this.context.closePath();
+                
+                this.context.stroke();
+                this.context.fill(); 
+            },
+
+
+            
             /**
              * Draws a rectangle
              * @param {number} x position at x axis
