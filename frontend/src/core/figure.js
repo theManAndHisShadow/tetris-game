54deaf491872a5cc62522be4047a8115e5e5a243
theManAndHisShadow <justemailpostbox@gmail.com>
2024-06-26 12:57:17 +0500

    [Block]: move Bloock class to dedicated file, Block class rewrited using ES6

--- frontend/src/core/figure.js
+++ frontend/src/core/figure.js
@@ -1,61 +1,7 @@
 console.log('[Log]: Starting figure.js');
 
 import { rotatePoint } from "../misc/helpers.js";
-import { Texture } from "./texture.js";
-
-const Block = function({id, x, y, color, size, renderer, parentFigureID, textureName} = {}){
-    // preparing texture
-    let blockTexture = new Texture({
-        renderer: renderer, 
-        width: size, 
-        height: size,
-        x: x,
-        y: y,
-    });
-
-    // preload texture file
-    blockTexture.load(`../../resources/images/textures/${textureName}`);
-
-    return {
-        id: id,
-        x: x,
-        y: y,
-        color: color,
-        size: size,
-        parentFigureID: parentFigureID,
-
-        renderer: renderer,
-        texture: blockTexture,
-        textureName: textureName,
-
-
-        
-        /**
-         * Render block using texture rendering method
-         */
-        render: function(){
-            this.texture.x = this.x;
-            this.texture.y = this.y;
-            this.texture.render();
-        },
-
-
-
-        /**
-         * @param {object} from Figure Object ref
-         */
-        deleteFrom: function(from){
-            from.forEach(figure => {
-                if(figure.id == this.parentFigureID) {
-                    figure.blocks = figure.blocks.filter(block => {
-                        return block.id !== this.id;
-                    });
-                }
-            });
-        }
-    }
-};
-
+import { Block } from "./block.js";
 
 /**
  * 
